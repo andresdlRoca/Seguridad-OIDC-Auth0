692b003f668f933b846bde3e96c086f91c8684a7
jwlh00 <60154594+jwlh00@users.noreply.github.com>
2024-02-15 11:47:03 -0600

    setup api call

--- api/server.js
+++ api/server.js
@@ -1,58 +1,31 @@
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const { expressjwt } = require('express-jwt');
-const jwks = require('jwks-rsa');
-const axios = require('axios');
+const { expressjwt: jwt } = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 const app = express();
-app.use(cors());
 
-//Middleware to validate the token
-const verifyJwt = expressjwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://dev-spdnexpuf8hzfcfu.us.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'unique identifier',
-    issuer: 'https://dev-spdnexpuf8hzfcfu.us.auth0.com/',
-    algorithms: ['RS256'],
-}).unless({path: ['/']});
-
-app.use(verifyJwt);
-
-
-app.get('/', (req,res) => {
-    res.send("Hola desde la ruta principal / index");
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-spdnexpuf8hzfcfu.us.auth0.com/.well-known/jwks.json`,
+  }),
+  audience: `unique identifier`,
+  issuer: `https://dev-spdnexpuf8hzfcfu.us.auth0.com/`,
+  algorithms: ['RS256'],
 });
 
-app.get('/protected', async(req,res) => {
-    try{
-        const accessToken = req.headers.authorization.split(' ')[1];
-        const response = await axios.get('https://dev-spdnexpuf8hzfcfu.us.auth0.com/userinfo', {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-        const userInfo = response.data;
-        res.send(userInfo);
-    }
-    catch(error){
-        res.status(500).send(error.message);
-    }
+// Public endpoint
+app.get('/', (req, res) => {
+  res.send('Hello, this is a public endpoint!');
 });
 
-app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+// Protected endpoint
+app.get('/protected', checkJwt, (req, res) => {
+  res.send('Hello, this is a protected endpoint!');
 });
 
-app.use((error, req, res, next) => {
-    const status = error.status || 500;
-    const message = error.message || 'Internal Server Error';
-    res.status(status).send(message);
-});
-
-app.listen(4000, () => console.log("Server on port 4000"))
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`API listening on port ${port}`));
